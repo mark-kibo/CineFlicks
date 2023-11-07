d775f502680ac8dfc45c825da0c9dd4732ad1946
mark kibocha <63925315+mark-kibo@users.noreply.github.com>
2023-11-07 02:56:24 -0800

    Update runserver.py

--- backend/runserver.py
+++ backend/runserver.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+from api import create_app
 
-app = Flask(__name__)
+app = create_app()
 CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5000"}})
 
 
